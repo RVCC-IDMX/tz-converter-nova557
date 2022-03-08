2fade061ce61a6dbe714d2d2f91833da23568a50
nova557 <71886498+nova557@users.noreply.github.com>
2022-03-08 10:46:58 -0500

    Create an index.js file

---
+++ index.js
@@ -0,0 +1,19 @@
+ /*
+
+   whoa!    Hold your horses! This is nicely formatted code.
+
+*/
+
+
+
+const message =      "Hello World"
+
+
+
+
+
+
+
+
+console.log(message)            // this should not show as an error
+
